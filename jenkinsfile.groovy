d48d8e90c30d3a21d9daef8a906717fced062457
veeru786 <veeru.p786@gmail.com>
2017-08-29 20:31:58 +0530

    modified

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,7 +3,6 @@ stage 'Load a file from GitHub'
 def helloworld = fileLoader.fromGit('examples/fileLoader/helloworld', 
         'https://github.com/veeru786/remotefile.git', 'master', null, '')
 
-
 	stage 'Run method from the loaded file'
 	helloworld.printHello();
 	return this;
