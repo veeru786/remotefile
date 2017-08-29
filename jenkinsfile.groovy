e38b650d713e1220caab2ef9cd9b28e27c3fbe92
veeru786 <veeru.p786@gmail.com>
2017-08-29 20:27:44 +0530

    modified by file

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,7 +1,6 @@
 #!groovy
-stage 
-'Load a file from GitHub'
-def helloworld=  fileLoader.fromGit ('examples/fileLoader/helloworld', 
+stage 'Load a file from GitHub'
+def helloworld = fileLoader.fromGit('examples/fileLoader/helloworld', 
         'https://github.com/veeru786/remotefile.git', 'master', null, '')
 
 
